d6e6b538e415594ab4148bd657322a7bcc8e58b2
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-10-11 11:04:30 +0700

    Delete launch and kill commands

--- src/main/talky/core.cljs
+++ src/main/talky/core.cljs
@@ -106,79 +106,6 @@
   (when (get-in @*sys [:talky/connection :talky/connected?])
     (talk! *sys (str "(in-ns '" (document/ns-name (.-document editor)) ")"))))
 
-
-;; Launching a Socket Server
-;; https://clojure.org/reference/repl_and_main#_launching_a_socket_server
-
-;; Socket REPL design page
-;; https://dev.clojure.org/display/design/Socket+Server+REPL
-
-;; Clojure
-;; ~~~~~~~
-;; clojure -J-Dclojure.server.repl="{:port 5555 :accept clojure.core.server/io-prepl}"
-
-;; ClojureScript
-;; ~~~~~~~~~~~~~
-;; Node.js
-;; clojure -J-Dclojure.server.repl="{:port 5555 :accept cljs.server.node/prepl}"
-;;
-;; Browser
-;; clojure -J-Dclojure.server.repl="{:port 5555 :accept cljs.server.browser/prepl}"
-
-(defn ^{:cmd "talky.launchREPL"} launch-repl [*sys]
-  (let [clojure-repl       "Clojure REPL"
-        clojurescript-repl "ClojureScript REPL"
-        browser-repl       "Browser"
-        nodejs-repl        "Node.js"]
-    (p/let [repl-platform (gui/show-quick-pick [clojure-repl clojurescript-repl])
-
-            repl-env (when repl-platform
-                       (condp = repl-platform
-                         clojure-repl 'clojure.core.server/io-prepl
-                         clojurescript-repl (gui/show-quick-pick [browser-repl nodejs-repl])))
-
-            accept (when repl-env
-                     (condp = repl-env
-                       browser-repl 'cljs.server.browser/prepl
-                       nodejs-repl 'cljs.server.node/prepl
-                       repl-env))]
-      (when accept
-        (let [port    5555
-
-              config  `{:port ~port :accept ~accept}
-
-              deps '{:deps
-                     {org.clojure/clojure {:mvn/version "1.10.0-beta2"}
-                      org.clojure/clojurescript {:mvn/version "1.10.339"}}}
-
-              process (child-process/spawn "clojure" #js ["-Sdeps" (pr-str deps) (str "-J-Dclojure.server.repl=" config)])
-
-              _ (.once (.-stdout process) "data"
-                       (fn [data]
-                         (let [output-channel (get @*sys :talky/output-channel)]
-                           (.appendLine output-channel (str "REPL is running at port " port ".\n"))
-                           (.appendLine output-channel data)
-                           (.show output-channel true))))
-
-              _ (.on (.-stderr process) "data"
-                     (fn [data]
-                       (let [output-channel (get @*sys :talky/output-channel)]
-                         (.appendLine output-channel data)
-                         (.show output-channel true))))
-
-              _ (.on process "close"
-                     (fn [code]
-                       (let [output-channel (get @*sys :talky/output-channel)]
-                         (.appendLine output-channel (str "\nREPL exited with code " code ".\n"))
-                         (.show output-channel true))))]
-
-          (swap! *sys assoc :talky/repl {:talky.repl/port port
-                                         :talky.repl/process process}))))))
-
-(defn ^{:cmd "talky.killREPL"} kill-repl [*sys]
-  (when-let [^js process (get-in @*sys [:talky/repl :talky.repl/process])]
-    (.kill process)))
-
 (def *sys
   (atom {}))
 
@@ -195,12 +122,6 @@
          (register-disposable context))
 
     (->> (register-text-editor-command *sys #'switch-namespace-to-current-file)
-         (register-disposable context))
-
-    (->> (register-command *sys #'launch-repl)
-         (register-disposable context))
-
-    (->> (register-command *sys #'kill-repl)
          (register-disposable context))
 
     (reset! *sys {:talky/output-channel output-channel})
